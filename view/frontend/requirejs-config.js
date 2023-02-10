9b9b7bee145505eeabff22027dd34622baa05e6f
Nicolas Renault <nrenault@tangkoko.com>
2023-02-10 17:41:32 +0100

    Show Hide fields considering configured dependencies

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            camDependencies:            'Tangkoko_CustomerAttributesManagement/js/form/field/dependencies'
+        }
+    }
+};
---
+++ view/frontend/web/js/form/field/dependencies.js
@@ -0,0 +1,81 @@
+define([
+    'jquery',
+    'jquery/ui'
+], function ($) {
+    'use strict';
+
+    $.widget('cam.fielddependency', {
+        options: {
+            config: {},
+            model: {}
+        },
+
+        _create: function() {
+            const self = this;
+            this.model = this.options.model;
+            if(this.options.config.hasOwnProperty('conditions')){
+                this.conditions = this.options.config.conditions;
+                $.each(this.conditions, function(field, condition){
+                    self.parseCondition(field, condition, self);
+                    self.updateField(field);
+                });
+                
+            }
+        },
+        parseCondition: function(field, condition){
+            const self= this;
+            switch(condition.type){
+                case "Tangkoko\\CustomerAttributesManagement\\Model\\Rule\\Condition\\Combine":
+                    $.each(condition.conditions, function(index, subCondition){
+                        self.parseCondition(field, subCondition);
+                    });
+                break;
+                case "Tangkoko\\CustomerAttributesManagement\\Model\\Rule\\Condition\\Customer":
+                    $("input[name="+ condition.attribute +"],select[name="+ condition.attribute +"]").change(function(){
+                        self.updateField(field);
+                    });
+                break;
+            }
+        },
+        updateField: function(field){
+            if(this.isVisible(field)){
+                $("input[name="+ field +"],select[name="+ field +"]").closest("div.field").show();
+            }else{
+                $("input[name="+ field+"],select[name="+ field +"]").closest("div.field").hide();
+            }
+        },
+        isVisible: function(field){
+            return this.validateCondition(this.conditions[field]);
+        },
+        validateCondition: function(condition){
+            const self = this;
+            switch(condition.type){
+                case "Tangkoko\\CustomerAttributesManagement\\Model\\Rule\\Condition\\Combine":
+                    let result  =true;
+                    $.each(condition.conditions, function(index, subCondition){
+                        if(Boolean(condition.value) !==  Boolean(self.validateCondition(subCondition))){
+                            result = false;
+                        }
+                    });
+
+                    return result;
+                break;
+                case "Tangkoko\\CustomerAttributesManagement\\Model\\Rule\\Condition\\Customer":
+                    const value = $("input[name="+ condition.attribute +"],select[name="+ condition.attribute +"]").val();
+                    switch(condition.operator){
+                        case "()":
+                        case "{}":
+                            return condition.value.includes(value);
+                        break;
+                        case "!()":
+                        case "!{}":
+                            return !condition.value.includes(value);
+                        break;
+                    }
+                break;
+            }
+        }
+    });
+
+    return $.cam.fielddependency;
+});
